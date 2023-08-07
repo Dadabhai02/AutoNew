6bbeb75a9a737bb1d0ca1d5e8b7d4371ab0081a8
DaDa Bhai <120179611+Dadabhai02@users.noreply.github.com>
2023-08-07 20:57:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -270,7 +270,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🇯 🇴 🇮 🇳  🇳 🇴 🇼 ', url='https://t.me/dada_linkz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('𝖩𝗈𝗂𝗇 𝖬𝗈𝗋𝖾 🖇️', url='https://t.me/dada_linkz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
