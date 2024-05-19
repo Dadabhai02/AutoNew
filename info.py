2f5dc6d1aee1dde12d0bf06014b0159a8bd4d22f
DaDa Bhai <120179611+Dadabhai02@users.noreply.github.com>
2024-05-19 17:05:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'shorturllinks.com') #Put Only Shortner Site domain don't put like this https://tnlink.in/
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'hypershort.com') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', '138545371bfff0b5aa2328f6ac12b5ee7ada0365')
+SHORTNER_API =  environ.get('SHORTNER_API', '4d8f020aa131267a0b88ff1fa47c2f11555eba85')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
